3abe3f939eed1395ae801dc19dd5d095e117b327
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2020-03-15 08:09:20 +0200

    Updates

---
+++ precache-manifest.f4942fcfdc013d71fd5c992070b944ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2264b02851431656b00234d8b54e7af3",
+    "url": "/test-reactivate/index.html"
+  },
+  {
+    "revision": "ea82d33c18287af62f51",
+    "url": "/test-reactivate/static/js/2.ac2f7847.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/test-reactivate/static/js/2.ac2f7847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9b170a9088c76b69c9",
+    "url": "/test-reactivate/static/js/main.4478eeb7.chunk.js"
+  },
+  {
+    "revision": "2520d138ec9af5d490d2",
+    "url": "/test-reactivate/static/js/runtime-main.7f465c9f.js"
+  }
+]);
